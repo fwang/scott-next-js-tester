9c3918d13ea40f6c4e882c2a2cd43f72c0a72346
Scott-Lasertrade <scott@lasertrade.com.au>
2022-05-24 11:11:30 +1000

    Bring back amplify

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -16,45 +16,45 @@ const getAuth = async () => {
   }
 };
 
-// const configuration = {
-//   Auth: {
-//     region: config.cognito.REGION,
-//     userPoolId: config.cognito.USER_POOL_ID,
-//     identityPoolId: config.cognito.IDENTITY_POOL_ID,
-//     userPoolWebClientId: config.cognito.APP_CLIENT_ID,
-//     mandatorySignIn: false,
-//     oauth: {
-//       domain: `${
-//         config.cognito.OAUTH_DOMAIN +
-//         ".auth." +
-//         config.cognito.REGION +
-//         ".amazoncognito.com"
-//       }`,
-//       scope: ["email", "profile", "openid", "aws.cognito.signin.user.admin"],
-//       // TODO: Replace when env vars are fixed
-//       redirectSignIn: "http://localhost:3000",
-//       redirectSignOut: "http://localhost:3000",
-//       responseType: "token",
-//     },
-//   },
-//   API: {
-//     endpoints: [
-//       {
-//         name: "api",
-//         endpoint: config.apiGateway.URL,
-//         region: config.apiGateway.REGION,
-//         custom_header: async () => {
-//           // Alternatively, with Cognito User Pools use this:
-//           return {
-//             Authorization: await getAuth(),
-//           };
-//         },
-//       },
-//     ],
-//   },
-//   ssr: true,
-// };
-// Amplify.configure(configuration);
+const configuration = {
+  Auth: {
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    mandatorySignIn: false,
+    oauth: {
+      domain: `${
+        config.cognito.OAUTH_DOMAIN +
+        ".auth." +
+        config.cognito.REGION +
+        ".amazoncognito.com"
+      }`,
+      scope: ["email", "profile", "openid", "aws.cognito.signin.user.admin"],
+      // TODO: Replace when env vars are fixed
+      redirectSignIn: "http://localhost:3000",
+      redirectSignOut: "http://localhost:3000",
+      responseType: "token",
+    },
+  },
+  API: {
+    endpoints: [
+      {
+        name: "api",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+        custom_header: async () => {
+          // Alternatively, with Cognito User Pools use this:
+          return {
+            Authorization: await getAuth(),
+          };
+        },
+      },
+    ],
+  },
+  ssr: true,
+};
+Amplify.configure(configuration);
 console.log(configuration);
 
 function MyApp({ Component, pageProps }) {
